44163d627f4b30ea831b964fa7abb13418ef4246
M3r0dach <crosstimelove@hotmail.com>
2019-02-09 18:23:24 +0800

    add navbar

--- src/index.js
+++ src/index.js
@@ -1,38 +1,48 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
+import "./styles.css";
 
 const PrimaryLayout = match => (
   <div className="primary-layout">
-    <header>
-      Our React Router 4 App
-      <Route path={match.path} component={UsersMenu} />
-    </header>
-
+    <PrimaryHeader />
     <main>
       <Route path="/" exact component={HomePage} />
-      <Route path="/user" component={UsersPage} />
+      <Route path="/user" component={UsersSubLayout} />
     </main>
   </div>
 );
-const UsersMenu = () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/User">User</Link>
-    </li>
-  </ul>
+const PrimaryHeader = () => (
+  <header className="primary-header">
+    <h1>Welcome to our app!</h1>
+    <nav>
+      <NavLink to="/" exact activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink to="/user" activeClassName="active">
+        Users
+      </NavLink>
+    </nav>
+  </header>
 );
 const HomePage = () => <h1>Home Page</h1>;
 const UsersProfile = ({ match }) => <div>{match.params.id}</div>;
 const User = () => <h1>User Page</h1>;
-const UsersPage = ({ match }) => (
-  <Switch>
-    <Route path="/User" exact component={User} />
-    <Route path={`${match.path}/:id`} component={UsersProfile} />
-  </Switch>
+const UsersSubLayout = ({ match }) => (
+  <div className="user-sub-layout">
+    <aside>
+      <nav>
+        <NavLink to={match.path + "/1"}>1</NavLink>
+        <NavLink to={match.path + "/2"}>2</NavLink>
+      </nav>
+    </aside>
+    <div className="primary-content">
+      <Switch>
+        <Route path={match.path} exact component={User} />
+        <Route path={`${match.path}/:id`} component={UsersProfile} />
+      </Switch>
+    </div>
+  </div>
 );
 
 const App = () => (
